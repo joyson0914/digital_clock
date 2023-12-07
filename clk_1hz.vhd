b32263f75943ff449581ab66cc88fb378fb22d11
Joy Son <98338109+joyson0914@users.noreply.github.com>
2023-12-07 13:35:05 -0500

    Add files via upload

---
+++ clk_1hz.vhd
@@ -0,0 +1,31 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity clk_1hz is
+  Port (
+  clk : in std_logic ;
+  clk1 : out std_logic 
+   );
+end clk_1hz;
+
+architecture Behavioral of clk_1hz is
+
+signal temp1: std_logic:='0';
+signal counter1: INTEGER:=0;
+
+begin
+process(clk)
+begin
+if rising_edge(clk) then
+counter1<=counter1+1;
+if (counter1 = 49999999) then
+temp1<= NOT temp1;
+counter1<=0;
+end if;
+end if;
+clk1<=temp1;
+end process;
+
+end Behavioral;
---
+++ clk_1khz.vhd
@@ -0,0 +1,29 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity clk_1khz is
+  Port (clk: in std_logic;
+  clk2: out std_logic   );
+end clk_1khz;
+
+architecture Behavioral of clk_1khz is
+
+signal temp2: std_logic :='0';
+signal counter2: INTEGER :=0;
+
+begin
+process(clk)
+begin
+if rising_edge (clk) then
+counter2<=counter2+1;
+if (counter2=49999) then
+temp2<= NOT temp2;
+counter2<=0;
+end if;
+end if;
+clk2<=temp2;
+end process;
+
+end Behavioral;
